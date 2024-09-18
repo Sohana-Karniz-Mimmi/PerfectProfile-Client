82961089e40ef90d056b53031392400008c20ead
Md.Rokonuzzaman Sayem <sayem99133@gmail.com>
2024-09-18 21:40:40 +0600

    signin and signup component create doen and authenticaiton work on going

---
+++ src/Authentication/SignUp.jsx
@@ -0,0 +1,47 @@
+import Container from "../Shared/Container";
+
+const SignUp = () => {
+  return (
+    <div>
+      <Container>
+        <div>
+          <form className="card-body">
+            <h1 className="text-5xl font-bold">Login now!</h1>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="email"
+                placeholder="email"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type="password"
+                placeholder="password"
+                className="input input-bordered"
+                required
+              />
+              <label className="label">
+                <a href="#" className="label-text-alt link link-hover">
+                  Forgot password?
+                </a>
+              </label>
+            </div>
+            <div className="form-control mt-6">
+              <button className="btn btn-primary">Login</button>
+            </div>
+          </form>
+        </div>
+      </Container>
+    </div>
+  );
+};
+
+export default SignUp;
