af8f7ed3956008ad5a19bbb0b923e61b19a0be75
Alomgir Hasan Shakib <alomgirhasanshakib@gmail.com>
2024-09-19 12:47:28 +0600

    center the heading text

--- src/Components/Testomonial/TestimonialCard.jsx
+++ src/Components/Testomonial/TestimonialCard.jsx
@@ -4,9 +4,9 @@ const TestimonialCard = ({ name, desc, img, title, prevSlide, nextSlide }) => {
   return (
     <section className="bg-white dark:bg-gray-900">
       <div className="max-w-6xl px-6 py-10 mx-auto">
-        <p className="text-xl font-medium text-blue-500 ">Testimonials</p>
+        <p className="text-xl font-medium text-blue-500 text-center">Testimonials</p>
 
-        <h1 className="mt-2 text-2xl font-semibold text-gray-800 capitalize lg:text-3xl dark:text-white">
+        <h1 className="mt-2 text-2xl font-bold text-gray-800 capitalize lg:text-4xl dark:text-white text-center">
           What clients saying
         </h1>
 
