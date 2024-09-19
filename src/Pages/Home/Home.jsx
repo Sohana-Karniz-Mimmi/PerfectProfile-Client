9a9b0df817e9e3b218c280deede307a69539a3d8
humashaaa <afridakhan2121@gmail.com>
2024-09-19 17:30:28 +0600

    updated package section

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,7 +3,7 @@ import Package from "../../Components/Package/Package";
 
 const Home = () => {
   return (
-    <div className=" py-4">
+    <div className="bg-black text-white py-4">
       This Is Home page
       <Package></Package>
     </div>
