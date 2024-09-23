91e5096bce3f442842c7d485b4d5e0645f9c3422
DeveloperFahad <developerfahad.me@gmail.com>
2024-09-23 21:52:39 +0600

    Created custom shape for template section using raw css

--- src/Components/Home/Banner.jsx
+++ src/Components/Home/Banner.jsx
@@ -2,6 +2,8 @@ import { Link } from "react-router-dom";
 import video from "../../assets/banner.mp4";
 import { Typewriter } from "react-simple-typewriter";
 import { initializeApp } from "firebase/app";
+import Container from "../../Shared/Container";
+import "./Banner.css"
 
 const Banner = () => {
   return (
@@ -9,7 +11,7 @@ const Banner = () => {
       <section className="relative bg-white">
         {/* <div className="absolute inset-0 "></div> */}
 
-        <div className="relative mx-auto max-w-screen-xl px-4 pb-32 sm:px-6 lg:flex justify-between lg:h-screen lg:items-center lg:px-8">
+        <div className="relative mx-auto max-w-screen-xl px-4 pb-14  sm:px-6 lg:flex justify-between lg:items-center lg:px-8">
           <div className="max-w-xl ltr:sm:text-left rtl:sm:text-right">
             <h1 className="lg:text-5xl md:text-3xl text-2xl font-bold font-lora">
               Make your professional
@@ -33,21 +35,35 @@ const Banner = () => {
 
             <div className="mt-8 flex flex-wrap gap-6 text-center">
               <Link>
-                <button className="py-3 px-5 rounded font-bold font-roboto text-base bg-primary text-white ">Get Started</button>
+                <button className="py-3 px-5 rounded font-bold font-roboto text-base bg-primary text-white ">
+                  Get Started
+                </button>
               </Link>
 
               <Link>
-                <button className="py-3 px-5 rounded font-bold font-roboto text-base border border-primary text-primary hover:bg-primary hover:text-white ">Browse Templates</button>
+                <button className="py-3 px-5 rounded font-bold font-roboto text-base border border-primary text-primary hover:bg-primary hover:text-white ">
+                  Browse Templates
+                </button>
               </Link>
             </div>
           </div>
-          <div className="md:w-[600px]">
+          <div className="md:w-[500px]">
             <video width="500" autoPlay muted loop>
               <source src={video} type="video/mp4" />
               Your browser does not support the video tag.
             </video>
           </div>
         </div>
+      </section>
+      <section className="thing">
+        <Container>
+          <div className="flex flex-col justify-center items-center space-y-4">
+            <h1 className="text-5xl font-bold text-center font-lora text-white">
+              Templates to win recruiters over
+            </h1>
+            <p className="font-montserrat text-center lg:w-3/4 w-full">We'll help you choose the right layout for your CV from over 40 available templates. Each is instantly ready to use and requires no design skills.</p>
+          </div>
+        </Container>
       </section>
     </>
   );
