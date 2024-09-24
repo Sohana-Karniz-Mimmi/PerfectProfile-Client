e8eda9971f641bfaa06a8d092e07c1021fd26a30
DeveloperFahad <developerfahad.me@gmail.com>
2024-09-25 02:27:06 +0600

    Added template slider section on homepage.

--- src/Components/TemplateSlider/TemplateSlider.jsx
+++ src/Components/TemplateSlider/TemplateSlider.jsx
@@ -1,53 +1,3 @@
-// import React, { useRef, useState } from 'react';
-// // Import Swiper React components
-// import { Swiper, SwiperSlide } from 'swiper/react';
-
-// // Import Swiper styles
-// import 'swiper/css';
-// import 'swiper/css/pagination';
-
-// import './styles.css';
-
-// import Tem1 from '../../assets/1.png';
-// import Tem2 from '../../assets/2.png';
-// import Tem3 from '../../assets/3.png';
-// import Tem4 from '../../assets/4.png';
-// import Tem5 from '../../assets/5.png';
-// import Tem6 from '../../assets/6.png';
-
-// // import required modules
-// import {Autoplay, Pagination, Navigation } from 'swiper/modules';
-
-// export default function App() {
-//   return (
-//     <>
-//       <Swiper
-//         slidesPerView={5}
-//         spaceBetween={30}
-//         loop={true}
-//         autoplay={{
-//             delay: 1000,
-//             disableOnInteraction: false,
-//           }}
-//         pagination={{
-//           clickable: true,
-//         }}
-//         modules={[Autoplay, Pagination, Navigation]}
-//         className="mySwiper"
-//       >
-//         <SwiperSlide><img src={Tem1} alt="" /></SwiperSlide>
-//         <SwiperSlide><img src={Tem2} alt="" /></SwiperSlide>
-//         <SwiperSlide><img src={Tem3} alt="" /></SwiperSlide>
-//         <SwiperSlide><img src={Tem4} alt="" /></SwiperSlide>
-//         <SwiperSlide><img src={Tem5} alt="" /></SwiperSlide>
-//         <SwiperSlide><img src={Tem6} alt="" /></SwiperSlide>
-//         {/* <SwiperSlide>Slide 7</SwiperSlide>
-//         <SwiperSlide>Slide 8</SwiperSlide>
-//         <SwiperSlide>Slide 9</SwiperSlide> */}
-//       </Swiper>
-//     </>
-//   );
-// }
 
 import React from 'react';
 // Import Swiper React components
@@ -75,10 +25,8 @@ export default function App() {
       <Swiper
         loop={true}
         spaceBetween={30}
-        autoplay={{
-          delay: 1000,
-          disableOnInteraction: false,
-        }}
+        // 
+        navigation={true}
         pagination={{
           clickable: true,
         }}
@@ -95,7 +43,7 @@ export default function App() {
           },
         }}
         modules={[Autoplay, Pagination, Navigation]}
-        className="mySwiper z-10 absolute"
+        className="mySwiper"
       >
         <SwiperSlide><img src={Tem1} alt="" /></SwiperSlide>
         <SwiperSlide><img src={Tem2} alt="" /></SwiperSlide>
