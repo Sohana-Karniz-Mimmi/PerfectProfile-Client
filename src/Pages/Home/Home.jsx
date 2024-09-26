ebdfa39640075a684e082ab9836b6015af29c42b
Sohana-Karniz-Mimmi <sohanasheikhmimmi@gmail.com>
2024-09-26 21:12:48 +0600

    fixed some issue

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,6 @@
 import Banner from "../../Components/Home/Banner";
 import PremiumFeature from "../../Components/Home/PremiumFeature";
-import Package from "../../Components/Package/Package";
 import Testomonial from "../../Components/Testomonial/Testomonial";
-// import ResumeTemplates from "../../Components/TemplateSection/ResumeTemplates";
-// import CVResume from "../../Components/TemplateSection/CVResume";
 
 
 
@@ -16,11 +13,9 @@ const Home = () => {
       <div>
         <PremiumFeature />
       </div>
-      <Package />
       <div>
         <Testomonial></Testomonial>
       </div>
-      {/* <ResumeTemplates></ResumeTemplates> */}
       
 
     </div>
