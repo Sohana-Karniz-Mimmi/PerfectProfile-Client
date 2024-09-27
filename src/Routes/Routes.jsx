5398b389aaceb585fb8d991f2fdf88137fb36a8c
humashaaa <afridakhan2121@gmail.com>
2024-09-27 12:32:34 +0600

    fixed a issue

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -33,6 +33,14 @@ const router = createBrowserRouter([
         path: "/predefined-templates",
         element: <Template/>
       },
+      {
+        path: "/signIn",
+        // element: <SignIn />,
+      },
+      {
+        path: "/SignUp",
+        // element: <SignUp />,
+      },
      
       {
         path: "/contact",
