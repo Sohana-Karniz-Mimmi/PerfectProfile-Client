09b71ff6fe31e52adbeb0ab46f2341a62c7abdf2
humashaaa <afridakhan2121@gmail.com>
2024-10-09 16:21:53 +0600

    pdf modal

--- src/Pages/FinalResume/FinalResume.jsx
+++ src/Pages/FinalResume/FinalResume.jsx
@@ -10,6 +10,8 @@ import { FaFileExport, FaShare } from "react-icons/fa6";
 import useAxiosPublic, { axiosPublic } from "../../Hook/useAxiosPublic";
 import { ResumeContext } from "../../Context/CustomizeResumeContext";
 import ShareLinkCopyModal from "./ShareLinkCopyModal";
+import { Menu } from '@headlessui/react';
+import { GrDocumentText } from "react-icons/gr";
 import axios from "axios";
 const FinalResume = () => {
   const axiosPublic = useAxiosPublic()
@@ -105,9 +107,39 @@ const FinalResume = () => {
         
      
         <div className="w-2/12 flex flex-col items-start px-7 pt-10 gap-4">
-          <button onClick={handlePdf}  className="w-36 px-8 border font-montserrat rounded-full text-center border-secondary text-secondary flex items-center gap-2">
+         
+
+          <div className="relative text-right">
+            <Menu as="div" className="relative inline-block text-left ">
+                <Menu.Button className="btn btn-ghost btn-circle avatar text-black">
+                <button   className="w-36 px-8 border font-montserrat rounded-full text-center border-secondary text-secondary flex items-center gap-2">
             <FaFileExport className="text-secondary" /> Export
           </button>
+                </Menu.Button>
+
+                <Menu.Items
+                    className="absolute right-0 mt-2 w-40 origin-top-right p-[2px] bg-gradient-to-r from-[#00FFB2] via-[#00ffff] to-[#006AFF] rounded-xl shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none md:w-44"
+                >
+                    <div className="bg-white rounded-xl p-5">
+                        <Menu.Item>
+                            {({ active }) => (
+                                <button onClick={handlePdf}
+                                    className={`${active ? ' font-semibold bg-white ' : 'font-semibold'
+                                        } group flex w-full items-center gap-2  py-1.5 text-black`}
+                                >
+                                   <GrDocumentText className="text-primary" /> PDF Standard
+                                </button>
+                            )}
+                        </Menu.Item>
+                      
+                       
+                        
+                      
+                    </div>
+                </Menu.Items>
+
+            </Menu>
+        </div>
          
           <button
             onClick={() => setShareLinkCopy(true)}
