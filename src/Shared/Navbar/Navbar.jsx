7a3fb5ac44d2b3d42b3eb01cb4e09f2c56e7523d
Sohana-Karniz-Mimmi <sohanasheikhmimmi@gmail.com>
2024-10-09 14:21:15 +0600

    change navbar style

--- src/Shared/Navbar/Navbar.jsx
+++ src/Shared/Navbar/Navbar.jsx
@@ -124,10 +124,10 @@ const Navbar = () => {
   return (
     <div className="min-h-[99px] border-b shadow">
       <Container>
-        <div className=" navbar items-center justify-between barlow-regular min-h-[99px] p-0 md:py-3 py-5">
-          <div className="">
-            <details className="dropdown">
-              <summary className="m-1 btn bg-transparent border-none hover:bg-transparent lg:hidden shadow-none">
+        <div className="flex items-center justify-between min-h-[99px] p-0 md:py-3 py-5">
+          <div className="flex items-center">
+            <details className="relative">
+              <summary className="m-1 bg-transparent border-none lg:hidden shadow-none cursor-pointer">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   className="h-6 w-6 font-bold"
@@ -143,48 +143,47 @@ const Navbar = () => {
                   />
                 </svg>
               </summary>
-              <ul className="p-2 shadow dropdown-content z-50 bg-white rounded-box w-52 text-black space-y-2">
-                {navLinks}
+              <ul className="absolute mt-2 p-2 shadow-lg z-50 bg-white rounded-lg w-52 text-black space-y-2">
+                {navLinks} {/* Replace with your navigation links */}
               </ul>
             </details>
+
 
             <Link
               to={"/"}
               className="font-bold text-lg md:text-3xl gap-3 flex items-center"
             >
               {/* <img className="md:w-12 md:h-10 w-7 h-7 relative" src={logo} alt="" /> */}
-              <span className="">
-                P<span className="">er</span>fect
+              <span>
+                P<span>er</span>fect
                 <span className="text-primary">Profile</span>
               </span>
             </Link>
 
             {/* Nav Menu */}
-            <div className="navbar-center hidden ml-8 lg:flex">
-              <ul className="menu-horizontal space-x-5 ">{navLinks}</ul>
+            <div className="hidden ml-8 lg:flex">
+              <ul className="flex space-x-5">{navLinks}</ul>
             </div>
           </div>
 
           {/* Right Section (Login/Logout Buttons) */}
-          <div className="">
+          <div>
             {user ? (
               <div className="flex md:gap-5 items-center">
-                <Notification/>
-                <NavModal
-                handleLogoutBtn={handleLogoutBtn}
-                />
+                <Notification />
+                <NavModal handleLogoutBtn={handleLogoutBtn} />
               </div>
             ) : (
               <div className="flex gap-5">
                 <button
                   onClick={() => document.getElementById("my_modal_3").showModal()}
-                  className="font-bold font-montserrat flex gap-2 items-center justify-center py-2 bg-primary px-5 rounded-lg text-white"
+                  className="font-bold flex gap-2 items-center justify-center py-2 bg-primary px-5 rounded-lg text-white"
                 >
                   <FaUser className="text-sm text-white"></FaUser>Log In
                 </button>
 
                 <Link to={`/createResume`}>
-                  <button className="hidden font-bold flex gap-2 items-center justify-center py-2 lg:block bg-secondary font-montserrat text-white px-5 rounded-lg">
+                  <button className="hidden font-bold flex gap-2 items-center justify-center py-2 lg:block bg-secondary text-white px-5 rounded-lg">
                     Create My Resume
                   </button>
                 </Link>
@@ -197,9 +196,8 @@ const Navbar = () => {
       {/* Modal for Login */}
       <Login />
       <Register />
-      {/* <Modal>
-      </Modal> */}
     </div>
+
   );
 };
 
