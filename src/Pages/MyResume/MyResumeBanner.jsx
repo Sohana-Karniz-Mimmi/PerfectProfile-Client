b8510bfd21f04b64eefb5715c19a0474483ab492
Sohana-Karniz-Mimmi <sohanasheikhmimmi@gmail.com>
2024-10-15 23:04:14 +0600

    added a banner for my resume page

--- src/Pages/MyResume/MyResumeBanner.jsx
+++ src/Pages/MyResume/MyResumeBanner.jsx
@@ -1,7 +1,9 @@
 import img from "../../assets/pricing banner.png";
 const MyResumeBanner = () => {
   return (
-    <div className="flex flex-col lg:flex-row justify-between mb-52 lg:mb-7 items-center px-2 lg:px-9 h-80 lg:h-72 mt-9 py-6 rounded-lg bg-cyan-50">
+    <div className="flex flex-col lg:flex-row justify-between mb-52 lg:mb-7 items-center px-2 lg:px-9 h-80 lg:h-72 mt-9 py-6 rounded-lg bg-cyan-50" 
+    style={{ boxShadow: 'rgba(99, 99, 99, 0.2) 0px 2px 8px 0px' }}
+    >
       <div>
         <h1 className="text-4xl font-bold ">Manage Your Resume</h1>
         <p className="text-gray-500 w-80 lg:w-[44rem] mt-4">
