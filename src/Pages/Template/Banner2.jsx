71274100e52496751d095966d162947f5496bfc2
humashaaa <afridakhan2121@gmail.com>
2024-10-16 12:41:53 +0600

    filter for template page

---
+++ src/Pages/Template/Banner2.jsx
@@ -0,0 +1,17 @@
+
+const Banner2 = () => {
+    return (
+        <div className="mb-40 mt-36">
+            <h1 className="font-bold text-4xl text-center ">Why <span className=" border-b-4 rounded-sm border-primary">Choose</span> <span>
+                P<span>er</span>fect
+                <span className="text-primary">Profile</span>?
+              </span></h1>
+
+              <div>
+                
+              </div>
+        </div>
+    );
+};
+
+export default Banner2;
--- src/Pages/Template/Template.jsx
+++ src/Pages/Template/Template.jsx
@@ -16,6 +16,7 @@ import "./template.css";
 import { FaArrowLeft, FaArrowRight } from "react-icons/fa6";
 
 import toast from "react-hot-toast";
+import Banner2 from "./Banner2";
 const Template = () => {
   const axiosPublic = useAxiosPublic();
   const navigate = useNavigate();
@@ -27,13 +28,16 @@ const Template = () => {
   const [itemPerPage, setItemPerPage] = useState(4)
   const [currentPage, setCurrentPage] = useState(1)
   const [count, setCount] = useState(0)
+  const [filter, setFilter] = useState("all")
+  
   useEffect(() => {
+
     const getData = async () => {
-      const { data } = await axiosPublic(`/templates?page=${currentPage}&size=${itemPerPage}`);
+      const { data } = await axiosPublic(`/templates?page=${currentPage}&size=${itemPerPage}&filter=${filter}`);
       setPredefinedTemplate(data);
     };
     getData();
-  }, [currentPage, itemPerPage]);
+  }, [currentPage, itemPerPage, filter]);
 
   //   add to favorite
   const handleFavorite = (_id) => {
@@ -57,15 +61,16 @@ const Template = () => {
 
     console.log(_id);
   };
+
 // pagination
 
   useEffect(() => {
     const getCount = async () => {
-      const { data } = await axiosPublic(`/templates-count`);
+      const { data } = await axiosPublic(`/templates-count?filter=${filter}`);
       setCount(data.count)
     };
     getCount();
-  }, []);
+  }, [filter]);
 
   const numofPage = Math.ceil(count/itemPerPage)
   const pages = [...Array(numofPage).keys().map(element => element + 1)];
@@ -76,20 +81,9 @@ const Template = () => {
   }
 
   // filter
-  const [isOpen, setIsOpen] = useState(false);
-  const [selectedValue, setSelectedValue] = useState("Free + Premium"); 
+  
 
-  // Function to toggle the dropdown
-  const toggleDropdown = () => {
-    setIsOpen(!isOpen);
-  };
-
-  // Function to handle radio button selection and update the dropdown label
-  const handleChange = (event) => {
-    setSelectedValue(event.target.value);
-    setIsOpen(false); // Close the dropdown when an option is selected
-  };
-
+ 
   
   return (
     <Container>
@@ -98,74 +92,31 @@ const Template = () => {
       {/* <ResumeTemplates></ResumeTemplates> */}
 
       {/* filter */}
-
-   
-
       <div className="w-[79rem] mt-12 justify-end flex item-end">
             <select
-              name='category'
-              id='category'
+            onChange={e => setFilter(e.target.value)}
+              name='package'
+              id='package'
+              value={filter}
               className='border  px-4 py-2 rounded-lg '
             >
               {/* <option value=''>Filter By Category</option> */}
-              <option value='Free + Premium'>Free + Premium</option>
-              <option value='Free'>Free</option>
-              <option value='Premium'>Premium</option>
+              <option value='all'>Free + Premium</option>
+              <option value='free'>Free</option>
+              <option value='premium'>Premium</option>
             </select>
           </div>
-
-          
-      {/* <div className="w-[79rem] mt-12 justify-end flex item-end">
-      <div className="relative">
-      
-        <button
-          onClick={toggleDropdown}
-          className="border px-4 py-2 rounded-lg bg-white text-gray-800 hover:bg-primary hover:text-white"
-        >
-          {selectedValue} 
-        </button>
-
-        {isOpen && (
-          <ul className="absolute z-10 w-full mt-1 border rounded-lg bg-white">
-            <li
-              onClick={() => handleSelection('Free + Premium')}
-              className="px-4 py-2 hover:bg-primary hover:text-white cursor-pointer"
-            >
-              Free + Premium
-            </li>
-            <li
-              onClick={() => handleSelection('Free')}
-              className="px-4 py-2 hover:bg-primary hover:text-white cursor-pointer"
-            >
-              Free
-            </li>
-            <li
-              onClick={() => handleSelection('Premium')}
-              className="px-4 py-2 hover:bg-primary hover:text-white cursor-pointer"
-            >
-              Premium
-            </li>
-          </ul>
-        )}
-      </div>
-          </div> */}
           
 
       <div className="mt-12 grid grid-cols-3 px-32 gap-8">
         {predefinedTemplate?.map((template) => (
           <div key={template._id}>
             <div className="relative group w-[350px] h-[450px]">
-              {/* Favorite Button with Tooltip */}
-              {/* <button className="absolute text-3xl top-[6px] right-3 text-red-500 z-10 opacity-0 group-hover:opacity-100 transition-opacity duration-300 tooltip-favorite">
-          <GrFavorite />
-        </button> */}
+            
               <button
                 onClick={() => handleFavorite(template._id)}
                 className="absolute text-3xl top-[6px] right-2 rounded-full   z-10 opacity-0 group-hover:opacity-100 transition-opacity duration-300 tooltip-favorite"
               >
-                {/* {
-            isFavorite?   <FaRegStar className="text-primary"/> : <FaStar className="text-primary"/>
-        } */}
                 {favorites[template._id] ? (
                   <FaStar className="text-cyan-400" />
                 ) : (
@@ -248,23 +199,9 @@ const Template = () => {
           </div>
         </button>
       </div>
-      {/* <div className="flex justify-center items-center mt-20 mb-16">
-          <div className="flex gap-5">
-            <button
-              className="btn bg-primary text-white px-6 hover:bg-secondary"
-            >
-              <FaArrowLeft />
-            </button>
-            <p>
-                1 0f 3
-            </p>
-            <button
-              className="btn bg-primary text-white px-6 hover:bg-secondary"
-            >
-              <FaArrowRight />
-            </button>
-          </div>
-        </div> */}
+
+     <Banner2></Banner2>
+    
     </Container>
   );
 };
