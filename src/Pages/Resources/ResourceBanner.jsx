270b4f7f6d645f0cbcc4fc49714044ce4ab12a33
Md.Rokonuzzaman Sayem <sayem99133@gmail.com>
2024-10-22 08:31:31 +0600

    resource banner section work done plus ui design and responsive

--- src/Pages/Resources/ResourceBanner.jsx
+++ src/Pages/Resources/ResourceBanner.jsx
@@ -3,35 +3,31 @@ import teamImg from "../../assets/Resource/career/pexels-tima-miroshnichenko-545
 const ResourceBanner = () => {
   return (
     <section
-      className="text-white relative bg-cover bg-center bg-no-repeat h-[565px]"
+      className="text-white relative bg-cover bg-center bg-no-repeat h-[450px]" // Reduced height to 450px
       style={{
-        background:
-          "linear-gradient(to right, rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url(" +
-          teamImg +
-          ")",
+        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url(${teamImg})`, // Added opacity to the background
+        backgroundSize: "cover",
       }}
     >
-      <div className="absolute inset-0 sm:bg-transparent  sm:to-white/25 ltr:sm:bg-gradient-to-r rtl:sm:bg-gradient-to-l"></div>
+      <div className="absolute inset-0 sm:bg-transparent sm:to-white/25 ltr:sm:bg-gradient-to-r rtl:sm:bg-gradient-to-l"></div>
 
-      <div className="relative lg:h-screen lg:items-center lg:px-8">
-        <div className="flex items-center justify-center h-[400px]">
-          <div>
-            <p
-              data-aos="zoom-in"
-              data-aos-duration="1000"
-              data-aos-delay="200"
-              className=" text-center mt-24 max-w-lg sm:text-lg/relaxed font-montserrat"
-            >
-              Resume and CV Building
-            </p>
-            <h1
-              data-aos="fade-up"
-              data-aos-duration="500"
-              className="text-3xl sm:text-5xl mt-2 font-lora"
-            >
-              CAREER CENTERS
-            </h1>
-          </div>
+      <div className="relative h-full flex items-center justify-center">
+        <div className="">
+          <p
+            data-aos="zoom-in"
+            data-aos-duration="1000"
+            data-aos-delay="200"
+            className="text-center max-w-lg sm:text-lg/relaxed font-montserrat"
+          >
+            Resume and CV Building
+          </p>
+          <h1
+            data-aos="fade-up"
+            data-aos-duration="500"
+            className="text-3xl sm:text-5xl mt-2 font-lora"
+          >
+            CAREER CENTER
+          </h1>
         </div>
       </div>
     </section>
