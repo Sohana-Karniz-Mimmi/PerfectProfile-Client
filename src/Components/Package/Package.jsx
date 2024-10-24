4abfb5a2fe94bf6a65abd91d14426580c5756e03
DeveloperFahad <developerfahad.me@gmail.com>
2024-10-24 19:36:32 +0600

    Fixed pricing card hieght issue for all devices

--- src/Components/Package/Package.jsx
+++ src/Components/Package/Package.jsx
@@ -5,6 +5,7 @@ import { Link } from "react-router-dom";
 import { useState } from 'react'
 import CheckoutForm from "../Payment/CheckoutForm";
 import { Description, Dialog, DialogPanel, DialogTitle } from '@headlessui/react'
+import "./Package.css"
 
 // import PackageModal from "./PackageModal";
 
@@ -16,9 +17,9 @@ const Package = () => {
     <Container className="">
       <div
         id="price"
-        className="flex lg:flex-row flex-col justify-between  gap-8 lg:py-10 py-10"
+        className="flex lg:flex-row flex-col justify-between mt-8  gap-8 lg:py-10 py-10"
       >
-        <div className="space-y-7 card lg:w-1/3 w-full lg:h-[39rem] md:h-[41rem] h-[41rem]  px-3">
+        <div className="space-y-7 card lg:w-1/3 w-full lg:h-[43rem] md:h-[41rem] h-[41rem]  px-3">
           <h1 className="text-4xl font-lora font-bold">Our Pricing</h1>
           <p className="text-gray-600 font-montserrat">
             In PerfectProfile, users can choose from flexible plans that provide
@@ -29,12 +30,14 @@ const Package = () => {
 
           <Link
             to="/predefined-templates"
-            className="px-5 py-2.5 h-24 relative rounded group overflow-hidden  bg-cyan-50 border-2 border-[#2CACD5]  text-[#2CACD5] inline-block"
+            className="px-5 py-2.5 h-24 relative flex justify-center items-center rounded group overflow-hidden  bg-cyan-50 border-2 border-[#2CACD5]  text-[#2CACD5] "
           >
-            <span className="absolute top-0 left-0 flex w-full h-0 mb-0 transition-all duration-200 ease-out transform translate-y-0 bg-[#2CACD5] group-hover:h-full opacity-90"></span>
+            <span className="absolute top-0 left-0 flex justify-center items-center w-full h-0 mb-0 transition-all duration-200 ease-out transform translate-y-0 bg-[#2CACD5] group-hover:h-full opacity-90"></span>
             <span className="relative group-hover:text-white ">
               <div className="flex flex-col items-center justify-center">
-                <p className="font-bold text-xl font-montserrat mt-2">Lets Build Resume</p>
+                <p className="font-bold text-xl font-montserrat mt-2">
+                  Lets Build Resume
+                </p>
                 <p className="font-medium text-center w-3/4 mx-auto">
                   With Unlimited Predefined Templates
                 </p>
@@ -78,7 +81,7 @@ const Package = () => {
                 Build, customize, and optimize your resume effortlessly to stand
                 out from the competition!
               </p>
-              <ul className="px-5 mt-2 font-roboto">
+              <ul className="px-5 font-roboto">
                 <li className="font-medium text-gray-600 flex items-baseline gap-2">
                   <SiTicktick className="mt-1 text-secondary" />
                   <span>Unlimited access to professional templates</span>
@@ -97,7 +100,7 @@ const Package = () => {
                 </li>
               </ul>
             </div>
-            <div className=" justify-center mt-4">
+            <div className="buy-btn justify-center lg:mt-24 mt-4">
               {/* standard button */}
 
               <button
@@ -148,7 +151,7 @@ const Package = () => {
 
           <div className="">
             <div className="flex  items-center lg:gap-16 gap-8 px-5 relative">
-              <h2 className="font-extrabold text-3xl font-roboto text-blue-950 mb-3">
+              <h2 className="font-extrabold text-3xl font-roboto text-blue-950 mb-2">
                 Yearly Pack
               </h2>
               <p className="px-2 absolute right-4 mb-3 py-1 bg-gradient-to-r text-center from-primary to-secondary hover:bg-gradient-to-l text-white rounded-full shadow-lg font-montserrat md:font-medium font-light md:text-base text-xs">
@@ -156,10 +159,10 @@ const Package = () => {
               </p>
             </div>
             <div>
-              <p className=" text-gray-600 mb-9 mt-2 font-montserrat px-5 ">
-                Enjoy all premium features while saving 20%! Unlock all premium
-                features with our affordable yearly package. Build, customize,
-                and optimize your resume effortlessly!
+              <p className=" text-gray-600 mb-4 mt-2 font-montserrat px-5 ">
+                Unlock all premium features with our affordable monthly package.
+                Build, customize, and optimize your resume effortlessly to stand
+                out from the competition!
               </p>
               <ul className="px-5 font-roboto">
                 <li className="font-medium text-gray-600 flex items-baseline gap-2">
@@ -180,7 +183,7 @@ const Package = () => {
                 </li>
               </ul>
             </div>
-            <div className="px-5 justify-center mt-4">
+            <div className="px-5 buy-btn justify-center lg:mt-24 mt-4">
               <button
                 onClick={() => set2ndIsOpen(true)}
                 class="relative inline-block text-lg group"
