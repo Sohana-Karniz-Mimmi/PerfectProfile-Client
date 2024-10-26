42062ca4c628d16ae0b57433a416140721356f15
humashaaa <afridakhan2121@gmail.com>
2024-10-26 12:39:20 +0600

    update consultant profile complete

--- src/Pages/ConsultantDashboard/Profile.jsx
+++ src/Pages/ConsultantDashboard/Profile.jsx
@@ -1,9 +1,16 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import useAuth from "../../Hook/useAuth";
 import { FiEdit } from "react-icons/fi";
-
+import img from "../../assets/consultation/profile.png";
+import { TiCameraOutline } from "react-icons/ti";
+import useAxiosPublic from "../../Hook/useAxiosPublic";
+import toast from "react-hot-toast";
+const img_hosting_key = import.meta.env.VITE_IMAGE_HOSTING_KEY;
+const img_hosting_api = `https://api.imgbb.com/1/upload?key=${img_hosting_key}`;
 const Profile = () => {
     const {user} = useAuth()
+    const axiosPublic = useAxiosPublic()
+    const [image, setImage] = useState(null)
 
     const formRef = useRef(null);
 
@@ -12,29 +19,116 @@ const Profile = () => {
       formRef.current.reset();
     };
 
+    const handleImageUpload = async (e) => {
+      const file = {image : e.target.files[0]};
+  
+      const res = await axiosPublic.post(img_hosting_api, file, {
+        headers: {
+          "content-Type": "multipart/form-data",
+        },
+      });
+      console.log(res.data);
+      // console.log(data?.image);
+  
+      if (res.data.success) {
+        const image = res.data.data.display_url;
+  console.log("image",image)
+        setImage(image);
+      }
+      
+    };
+
     // update profile data in db
-    const handleSubmit =(e)=>{
+    const handleSubmit =async(e)=>{
         e.preventDefault()
+        const form = e.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const number = form.number.value;
+    const address = form.address.value;
+    const expertise = form.expertise.value;
+    const experience = form.experience.value;
+    const facebook = form.facebook.value;
+    const about = form.about.value;
+    const twitter = form.twitter.value;
+    const linkdin = form.linkdin.value;
+ 
+        const consultantData = {
+          name,
+          email,
+          number,
+          experience,
+          expertise,      
+          address,
+          facebook,
+          about,
+          twitter,
+          linkdin,
+          image    
+        }
+      
+    console.log(consultantData)
 
 
-    }
+       axiosPublic.patch(`/consultant-info-update/user/${user?.email}`, consultantData)
+      .then(res => {
+          console.log(res.data);
+          toast.success("Your information has been updated");
+        });
+ }
+
+
+
+  
 
 
   return (
     <div className="min-h-screen">
       <div className="mx-auto  px-10">
-        <div >
+       <form 
+        ref={formRef}
+        onSubmit={handleSubmit}
+        className="w-full  mt-6  flex flex-col gap-3 "
+       >
+       <div className="flex justify-start items-center gap-[34rem] ">
+       <div >
           <h1 className="text-3xl font-bold text-secondary flex justify-center items-center gap-3 font-lora">Edit Profile <FiEdit /></h1>
           <img src="" alt="" />
         </div>
 
+        {/* img */}
+
+ 
+       <div className="relative ">
+       <img
+              className="rounded-full  lg:w-[10rem] h-40 w-36 cursor-pointer"
+              src={ img}
+              alt=""
+              id="profile"
+              onClick={() => document.getElementById("imageUpload").click()}
+            />
+            <TiCameraOutline className="absolute text-gray-600 text-4xl top-24" />
+
+            <input
+              type="file"
+              id="imageUpload"
+              name="image"
+              style={{ display: "none" }}
+              accept="image/*"
+              onChange={handleImageUpload}
+            />
+       </div>
+
+
+
+
+
+
+       </div>
+
         {/* form */}
 
-        <form
-        ref={formRef}
-          onSubmit={handleSubmit}
-          className="w-full  mt-6  flex flex-col gap-3"
-        >
+        
           {/* basic info */}
           <h1 className="mt-3 mb-1 font-semibold text-xl">
             Personal Information :
@@ -128,6 +222,7 @@ const Profile = () => {
                   </label>
                   <textarea
                     id="about"
+                    name="about"
                     
                     className="mt-1 block w-[424px] px-3 py-2 border border-secondary rounded-md shadow-sm focus:outline-none focus:ring-primary focus:border-primary sm:text-sm"
                     placeholder="Enter your message"
@@ -243,21 +338,23 @@ const Profile = () => {
             </div>
           </div>
 
-          <div className="mt-12 flex justify-end  items-center ml-[29rem]  w-[27rem] gap-7 ">
+          <div className="mt-12 flex justify-end  items-start ml-[29rem]  w-[27rem] gap-7 ">
             <button
               onClick={handleReset}
-              className="py-2 font-bold rounded-md w-full border-secondary text-secondary hover:bg-secondary border-2 hover:border hover:border-secondary hover:text-white "
+              className="py-2 font-bold rounded-md w-full border-secondary text-secondary hover:bg-secondary border hover:border hover:border-secondary hover:text-white "
             >
              Cancel
             </button>
             <button
               type="submit"
-              className="py-2 font-bold rounded-md w-full bg-secondary text-white hover:bg-transparent border-2 hover:text-secondary hover:border hover:border-secondary "
+              className="py-2 font-bold rounded-md w-full bg-secondary text-white hover:bg-transparent border hover:text-secondary hover:border hover:border-secondary "
             >
               Save
             </button>
+           
           </div>
-        </form>
+        
+       </form>
       </div>
     </div>
   );
