df46e9b34ad424675a199e819e297bbf27bc179c
Sohana-Karniz-Mimmi <sohanasheikhmimmi@gmail.com>
2024-10-26 12:47:33 +0600

    fixed a bugh

--- src/Components/AllTemplates/Template2.jsx
+++ src/Components/AllTemplates/Template2.jsx
@@ -189,7 +189,7 @@ const Template2 = ({ data, userData, setUserData }) => {
                   <h2 className=" uppercase text-sm font-roboto font-bold  border-b border-black ">
                     About Me
                   </h2>
-                  <p className="mt-1 text-sm font-roboto break-words text-justify max-w-[450px]">
+                  <p className="mt-1 text-sm font-roboto break-words text-justify">
                     {userData?.careerObjective}
                   </p>
                 </section>
