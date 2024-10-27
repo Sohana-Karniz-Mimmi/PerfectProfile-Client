b05af21885e2170a73bd280ae789225ac69f3cf6
DeveloperFahad <developerfahad.me@gmail.com>
2024-10-28 02:58:35 +0600

    Updated footer design

--- src/Shared/Footer/Footer.jsx
+++ src/Shared/Footer/Footer.jsx
@@ -1,26 +1,31 @@
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
-import footer from "../../assets/footer.png"
+import footer from "../../assets/footer.png";
 
 const Footer = () => {
   return (
     <>
-      <footer className="relative bg-cover bg-center text-white bg-no-repeat z-10 pb-10 pt-10 bg-[#1C2733] lg:pb-4 lg:pt-[110px]" style={{ backgroundImage: `url(${footer})` }} >
+      <footer
+        className="relative bg-cover bg-center text-white bg-no-repeat z-10 pb-10 pt-10 bg-[#1C2733] lg:pb-4 lg:pt-[110px]"
+        style={{ backgroundImage: `url(${footer})` }}
+      >
         <div className="container mx-auto md:px-10 px-5 md:text-start text-center">
           <div className="-mx-4 flex flex-wrap">
             <div className="w-full px-4 sm:w-2/3 lg:w-3/12">
               <div className="mb-10 w-full">
-              <Link className="mb-6 inline-block max-w-[160px]">
-                  <Link to={'/'} className="font-bold text-3xl md:text-4xl gap-3 flex items-center">
-                    <span className="">
-                      P<span className="">er</span>fect
-                      <span className="text-primary ">Profile</span>
-                    </span></Link>
+                <Link className="inline-block max-w-[160px]">
+                  <Link to="/">
+                    <h1 className="text-white lg:text-2xl md:text-lg text-xl pb-9 font-extrabold font-lora">
+                      Perfect<span className="text-primary">Profile</span>
+                    </h1>
+                  </Link>
                 </Link>
-                <p className="mb-7 text-[#a1a6ab] text-body-color">
-                At PerfectProfile, we help job seekers craft stunning resumes with customizable templates and expert tips, empowering you to stand out in today's competitive job market.
+                <p className="mb-7 text-slate-300 font-montserrat text-body-color">
+                  At PerfectProfile, we help job seekers craft stunning resumes
+                  with customizable templates and expert tips, empowering you to
+                  stand out in today's competitive job market.
                 </p>
-                <p className="flex md:justify-start justify-center items-center text-sm font-medium text-dark dark:text-[#a1a6ab]">
+                <p className="flex md:justify-start justify-center font-montserrat items-center text-sm font-medium text-dark dark:text-[#a1a6ab]">
                   <span className="mr-3 text-primary">
                     <svg
                       width="20"
@@ -55,9 +60,12 @@ const Footer = () => {
               </div>
             </div>
 
-            <LinkGroup header="Resources">
+            <LinkGroup className="!font-lora" header="Resources">
               <NavLink link="/predefined-templates" label="Resume Examples" />
-              <NavLink link="/predefined-templates" label="Cover Letter Examples" />
+              <NavLink
+                link="/predefined-templates"
+                label="Cover Letter Examples"
+              />
               <NavLink link="/pricing" label="Pricing" />
               <NavLink link="/resource" label="Resource" />
             </LinkGroup>
@@ -76,13 +84,11 @@ const Footer = () => {
 
             <div className="w-full px-4 sm:w-1/2 lg:w-3/12">
               <div className="mb-10 w-full">
-                <h4 className="mb-9 text-lg font-semibold text-dark dark:text-white">
+                <h4 className="mb-9 text-lg font-lora font-semibold text-dark dark:text-white">
                   Follow Us On
                 </h4>
-                <div className="mb-6 flex md:justify-start justify-center  items-center">
-                  <Link
-                    className="mr-3 flex h-8 w-8 items-center justify-center rounded-full border border-stroke text-dark hover:border-primary hover:bg-primary hover:text-white dark:border-dark-3 dark:text-white dark:hover:border-primary sm:mr-4 lg:mr-3 xl:mr-4"
-                  >
+                <div className="mb-6 font-montserrat flex md:justify-start justify-center  items-center">
+                  <Link className="mr-3 flex h-8 w-8 items-center justify-center rounded-full border border-stroke text-dark hover:border-primary hover:bg-primary hover:text-white dark:border-dark-3 dark:text-white dark:hover:border-primary sm:mr-4 lg:mr-3 xl:mr-4">
                     <svg
                       width="8"
                       height="16"
@@ -92,9 +98,7 @@ const Footer = () => {
                       <path d="M7.43902 6.4H6.19918H5.75639V5.88387V4.28387V3.76774H6.19918H7.12906C7.3726 3.76774 7.57186 3.56129 7.57186 3.25161V0.516129C7.57186 0.232258 7.39474 0 7.12906 0H5.51285C3.76379 0 2.54609 1.44516 2.54609 3.5871V5.83226V6.34839H2.10329H0.597778C0.287819 6.34839 0 6.63226 0 7.04516V8.90323C0 9.26452 0.243539 9.6 0.597778 9.6H2.05902H2.50181V10.1161V15.3032C2.50181 15.6645 2.74535 16 3.09959 16H5.18075C5.31359 16 5.42429 15.9226 5.51285 15.8194C5.60141 15.7161 5.66783 15.5355 5.66783 15.3806V10.1419V9.62581H6.13276H7.12906C7.41688 9.62581 7.63828 9.41935 7.68256 9.10968V9.08387V9.05806L7.99252 7.27742C8.01466 7.09677 7.99252 6.89032 7.85968 6.68387C7.8154 6.55484 7.61614 6.42581 7.43902 6.4Z" />
                     </svg>
                   </Link>
-                  <Link
-                    className="mr-3 flex h-8 w-8 items-center justify-center rounded-full border border-stroke text-dark hover:border-primary hover:bg-primary hover:text-white dark:border-dark-3 dark:text-white dark:hover:border-primary sm:mr-4 lg:mr-3 xl:mr-4"
-                  >
+                  <Link className="mr-3 flex h-8 w-8 items-center justify-center rounded-full border border-stroke text-dark hover:border-primary hover:bg-primary hover:text-white dark:border-dark-3 dark:text-white dark:hover:border-primary sm:mr-4 lg:mr-3 xl:mr-4">
                     <svg
                       width="16"
                       height="12"
@@ -104,9 +108,7 @@ const Footer = () => {
                       <path d="M14.2194 2.06654L15.2 0.939335C15.4839 0.634051 15.5613 0.399217 15.5871 0.2818C14.8129 0.704501 14.0903 0.845401 13.6258 0.845401H13.4452L13.3419 0.751468C12.7226 0.258317 11.9484 0 11.1226 0C9.31613 0 7.89677 1.36204 7.89677 2.93542C7.89677 3.02935 7.89677 3.17025 7.92258 3.26419L8 3.73386L7.45806 3.71037C4.15484 3.61644 1.44516 1.03327 1.00645 0.587084C0.283871 1.76125 0.696774 2.88845 1.13548 3.59296L2.0129 4.90802L0.619355 4.20352C0.645161 5.18982 1.05806 5.96477 1.85806 6.52838L2.55484 6.99804L1.85806 7.25636C2.29677 8.45401 3.27742 8.94716 4 9.13503L4.95484 9.36986L4.05161 9.93346C2.60645 10.8728 0.8 10.8024 0 10.7319C1.62581 11.7652 3.56129 12 4.90323 12C5.90968 12 6.65806 11.9061 6.83871 11.8356C14.0645 10.2857 14.4 4.41487 14.4 3.2407V3.07632L14.5548 2.98239C15.4323 2.23092 15.7935 1.8317 16 1.59687C15.9226 1.62035 15.8194 1.66732 15.7161 1.6908L14.2194 2.06654Z" />
                     </svg>
                   </Link>
-                  <Link
-                    className="mr-3 flex h-8 w-8 items-center justify-center rounded-full border border-stroke text-dark hover:border-primary hover:bg-primary hover:text-white dark:border-dark-3 dark:text-white dark:hover:border-primary sm:mr-4 lg:mr-3 xl:mr-4"
-                  >
+                  <Link className="mr-3 flex h-8 w-8 items-center justify-center rounded-full border border-stroke text-dark hover:border-primary hover:bg-primary hover:text-white dark:border-dark-3 dark:text-white dark:hover:border-primary sm:mr-4 lg:mr-3 xl:mr-4">
                     <svg
                       width="16"
                       height="12"
@@ -116,9 +118,7 @@ const Footer = () => {
                       <path d="M15.6645 1.88018C15.4839 1.13364 14.9419 0.552995 14.2452 0.359447C13.0065 6.59222e-08 8 0 8 0C8 0 2.99355 6.59222e-08 1.75484 0.359447C1.05806 0.552995 0.516129 1.13364 0.335484 1.88018C0 3.23502 0 6 0 6C0 6 0 8.79263 0.335484 10.1198C0.516129 10.8664 1.05806 11.447 1.75484 11.6406C2.99355 12 8 12 8 12C8 12 13.0065 12 14.2452 11.6406C14.9419 11.447 15.4839 10.8664 15.6645 10.1198C16 8.79263 16 6 16 6C16 6 16 3.23502 15.6645 1.88018ZM6.4 8.57143V3.42857L10.5548 6L6.4 8.57143Z" />
                     </svg>
                   </Link>
-                  <Link
-                    className="mr-3 flex h-8 w-8 items-center justify-center rounded-full border border-stroke text-dark hover:border-primary hover:bg-primary hover:text-white dark:border-dark-3 dark:text-white dark:hover:border-primary sm:mr-4 lg:mr-3 xl:mr-4"
-                  >
+                  <Link className="mr-3 flex h-8 w-8 items-center justify-center rounded-full border border-stroke text-dark hover:border-primary hover:bg-primary hover:text-white dark:border-dark-3 dark:text-white dark:hover:border-primary sm:mr-4 lg:mr-3 xl:mr-4">
                     <svg
                       width="14"
                       height="14"
@@ -129,20 +129,26 @@ const Footer = () => {
                     </svg>
                   </Link>
                 </div>
-                <p className="text-base text-body-color dark:text-[#a1a6ab]">
-                  &copy; {new Date().getFullYear()} Perfect Profile
-                </p>
+                {/* <p className="text-base text-body-color dark:text-[#a1a6ab]">
+                  &copy;{new Date().getFullYear()} Perfect Profile
+                </p> */}
               </div>
             </div>
           </div>
 
           {/* Copyright */}
-          <div className="footer footer-center text-[#a1a6ab] pt-4 pb-3 border-t">
+          <div className="footer text-sm footer-center text-slate-300 pt-4 pb-3 border-t">
             <aside>
-              <p className='text-center'>Copyright © {new Date().getFullYear()} - All right reserved by Perfect Profile Ltd</p>
+              <p className="text-center">
+                Copyright ©{new Date().getFullYear()} - All Right Reserved by{" "}
+                <span className="font-lora text-sm  text-primary">
+                  <a className="hover:underline" href="/">
+                    <span className="text-white">Perfect</span>Profile
+                  </a>
+                </span>
+              </p>
             </aside>
           </div>
-
         </div>
         <div>
           <span className="absolute bottom-0 left-0 z-[-1]">
@@ -200,7 +206,6 @@ const Footer = () => {
             </svg>
           </span>
         </div>
-
       </footer>
     </>
   );
@@ -213,7 +218,7 @@ const LinkGroup = ({ children, header }) => {
     <>
       <div className="w-full px-4 sm:w-1/2 lg:w-2/12">
         <div className="mb-10 w-full">
-          <h4 className="mb-9 text-lg font-semibold text-dark dark:text-white">
+          <h4 className="mb-9 text-lg font-lora font-semibold text-dark dark:text-white">
             {header}
           </h4>
           <ul className="space-y-3">{children}</ul>
@@ -228,7 +233,7 @@ const NavLink = ({ link, label }) => {
     <li>
       <a
         href={link}
-        className="inline-block text-base hover:underline leading-loose text-body-color hover:text-primary dark:text-[#a1a6ab]"
+        className="inline-block text-base font-montserrat hover:underline leading-loose text-body-color hover:text-primary dark:text-[#a1a6ab]"
       >
         {label}
       </a>
@@ -238,9 +243,9 @@ const NavLink = ({ link, label }) => {
 
 LinkGroup.propTypes = {
   children: PropTypes.node,
-  header: PropTypes.node
+  header: PropTypes.node,
 };
 NavLink.propTypes = {
   link: PropTypes.node,
-  label: PropTypes.node
+  label: PropTypes.node,
 };
