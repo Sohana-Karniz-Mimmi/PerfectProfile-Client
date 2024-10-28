a9662eeb95147eafcabafddcdd9f7622e8446918
Md.Rokonuzzaman Sayem <sayem99133@gmail.com>
2024-10-28 08:26:03 +0600

    template customization image change

--- src/Components/Home/Feature.jsx
+++ src/Components/Home/Feature.jsx
@@ -4,25 +4,16 @@ import Container from "../../Shared/Container";
 
 const Feature = () => {
   const data = [
-    // {
-    //   feature: "Live Preview System",
-    //   image_url: "https://i.ibb.co.com/25xrSth/file.png",
-    //   description: (
-    //     <>
-    //       Instant feedback allows you to see real-time updates as you edit,
-    //       making <br /> it easy to tailor your resume with various templates and
-    //       styles.
-    //     </>
-    //   ),
-    // },
     {
       feature: "Template Customization",
+      // image_url:
+      //   "https://i.ibb.co.com/Fbt4X6Z/template-vs-custom-site-graphic-01.png",
       image_url: "https://i.ibb.co.com/6Zpw8Cy/curriculum.png",
       description: (
         <>
           Choose from a variety of templates to suit your personal style and
-          career goals.easy to tailor your resume with various templates and
-          styles.
+          career goals, making it easy to tailor your resume with various
+          templates and styles.
         </>
       ),
     },
@@ -47,54 +38,44 @@ const Feature = () => {
         </>
       ),
     },
-    // {
-    //   feature: "Multiple Export Options",
-    //   image_url: "https://i.ibb.co.com/gJWXG4Q/export.png",
-    //   description: (
-    //     <>
-    //       Our multiple export options ensure your resume is compatible with any{" "}
-    //       <br />
-    //       platform, making sharing and submitting easy.
-    //     </>
-    //   ),
-    // },
   ];
 
   return (
-    <Container>
-      <div className="programs-container ">
-        <h2 className="title font-bold">Our Features</h2>
-        <ul className="cards">
-          {data.map((item, index) => (
-            <li key={index} className="card " style={{ "--index": index + 1 }}>
-              <div className="card__content  lg:w-[1000px] mx-auto">
-                <div className="bg-white border-t-4 border-b-0 h-40 rounded-t-2xl border-primary">
-                  <h1 className="text-primary card-title text-2xl md:text-3xl lg:text-4xl font-bold mt-2 pt-14 pl-5 font-lora">
-                    {item?.feature}
-                  </h1>
+    <div className="programs-container ">
+      <h2 className="title font-bold">Our Features</h2>
+      <p className="text-gray-800 text-[15px] font-montserrat text-center mb-12">
+        Our features are designed to make resume building effortless and
+        impactful. From customizable templates to one-on-one guidance, and
+        seamless version control.
+      </p>
+      <ul className="cards">
+        {data.map((item, index) => (
+          <li key={index} className="card" style={{ "--index": index + 1 }}>
+            <div className="card__content   xl:w-[980px] mx-auto">
+              <div className="bg-white border-t-4 border-b-0 h-40 rounded-t-2xl border-primary">
+                <h1 className="text-primary card-title text-2xl md:text-3xl lg:text-4xl font-bold mt-2 pt-14 pl-5 font-lora">
+                  {item?.feature}
+                </h1>
+              </div>
+              <div className="flex flex-col lg:flex-row items-center md:px-5 space-y-5 lg:space-y-0 lg:space-x-5 h-auto lg:h-52">
+                <div className="text-center lg:text-left max-w-xs lg:max-w-none">
+                  <p className="card-description text-base md:text-lg break-words mt-2 pt-5 font-montserrat lg:w-5/6">
+                    {item.description}
+                  </p>
                 </div>
-                <div className="flex   md:px-5 space-y-5 lg:space-y-0 lg:space-x-5 h-52">
-                  <div className="text-center lg:text-left max-w-xs lg:max-w-none">
-                    <p className="card-description text-base md:text-lg break-words mt-2 pt-5 font-montserrat lg:w-5/6">
-                      {item.description}
-                    </p>
-                  </div>
-                  <div className="flex justify-center items-center mt-5 lg:mt-0">
-                    {" "}
-                    {/* Add mt-5 for spacing on smaller screens */}
-                    <img
-                      className="  w-[22rem] h-44 text-white -mt-52"
-                      src={item.image_url}
-                      alt={item.feature}
-                    />
-                  </div>
+                <div className="flex justify-center items-center mt-5 lg:mt-0">
+                  <img
+                    className="w-40 h-40 md:w-56 md:h-56 lg:w-[22rem] lg:h-44 -mt-0 lg:-mt-52"
+                    src={item.image_url}
+                    alt={item.feature}
+                  />
                 </div>
               </div>
-            </li>
-          ))}
-        </ul>
-      </div>
-    </Container>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
