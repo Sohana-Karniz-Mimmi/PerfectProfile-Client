3ab5fde8be90840ebf0a2e0d4c1ca84c320fe578
Sohana-Karniz-Mimmi <sohanasheikhmimmi@gmail.com>
2024-10-29 15:30:43 +0600

    responsive main feature card

--- src/Components/Home/Feature.jsx
+++ src/Components/Home/Feature.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import "./Feature.css";
 import Container from "../../Shared/Container";
-import pdf from "../../assets/MainFeature Icon/icons8-pdf-100.png";
 
 const Feature = () => {
   const data = [
     {
       feature: "Live Preview System",
-      image_url: "https://i.ibb.co.com/25xrSth/file.png",
+      // image_url: "https://i.ibb.co.com/25xrSth/file.png",
+      // image_url: "https://i.ibb.co.com/F0RZ6Rb/Customization-2.png",
+      image_url: "https://i.ibb.co.com/yf0wz3B/11.png",
       description: (
         <>
           Instant feedback allows you to see real-time updates as you edit,
@@ -18,7 +19,11 @@ const Feature = () => {
     },
     {
       feature: "Template Customization",
-      image_url: "https://i.ibb.co.com/6Zpw8Cy/curriculum.png",
+      // image_url: "https://i.ibb.co.com/7pg0PbD/Customization-2.png",
+      image_url: "https://i.ibb.co.com/mBrsbXQ/Customization.png",
+      // image_url: "https://i.ibb.co.com/7pg0PbD/Customization-2.png",
+      // image_url: "https://i.ibb.co.com/ZK5gG5x/Version.png",
+      // image_url: "https://i.ibb.co.com/6Zpw8Cy/curriculum.png",
       description: (
         <>
           Choose from a variety of templates to suit your personal style and
@@ -29,7 +34,8 @@ const Feature = () => {
     },
     {
       feature: "One to One Guidance",
-      image_url: "https://i.ibb.co.com/30ZNZzq/operator.png",
+      // image_url: "https://i.ibb.co.com/30ZNZzq/operator.png",
+      image_url: "https://i.ibb.co.com/Kzqk9nm/Guidance-3.png",
       description: (
         <>
           Our consultancy service offers personalized advice to help you
@@ -40,7 +46,8 @@ const Feature = () => {
     },
     {
       feature: "Version Control",
-      image_url: "https://i.ibb.co.com/8KTpzw4/integration.png",
+      image_url: "https://i.ibb.co.com/Pj90SYM/Version-2.png",
+      // image_url: "https://i.ibb.co.com/8KTpzw4/integration.png",
       description: (
         <>
           Our version control feature allows you to easily revert to previous
@@ -50,7 +57,8 @@ const Feature = () => {
     },
     {
       feature: "Multiple Export Options",
-      image_url: 'https://i.ibb.co.com/QFMD4n3/1.png',
+      // image_url: 'https://i.ibb.co.com/QFMD4n3/1.png',
+      image_url: 'https://i.ibb.co.com/1MrynwS/13.png',
       description: (
         <>
           Our multiple export options ensure your resume is compatible with any
@@ -67,15 +75,15 @@ const Feature = () => {
         <ul className="cards">
           {data.map((item, index) => (
             <li key={index} className="card " style={{ "--index": index + 1 }}>
-              <div className="card__content  lg:w-[1000px] mx-auto">
+              <div className="card__content  xl:w-[1000px] mx-auto">
                 <div className="bg-white border-t-4 border-b-0 h-40 rounded-t-2xl border-primary">
                   <h1 className="text-primary card-title text-2xl md:text-3xl lg:text-4xl font-bold mt-2 pt-14 pl-5 font-lora">
                     {item?.feature}
                   </h1>
                 </div>
-                <div className="flex   md:px-5 space-y-5 lg:space-y-0 lg:space-x-5 h-52">
-                  <div className="text-center lg:text-left max-w-xs lg:max-w-none">
-                    <p className="card-description text-base md:text-lg break-words mt-2 pt-5 font-montserrat lg:w-5/6">
+                <div className="flex justify-between md:px-5 lg:pb-0 pb-7 lg:space-y-0 lg:space-x-5 lg:h-52">
+                  <div className="text-left max-w-xs lg:max-w-none">
+                    <p className="card-description text-base lg:text-[19px] break-words mt-2 pt-5 font-montserrat lg:w-5/6">
                       {item.description}
                     </p>
                   </div>
@@ -83,7 +91,7 @@ const Feature = () => {
                     {" "}
                     {/* Add mt-5 for spacing on smaller screens */}
                     <img
-                      className="  w-[21rem] h-40 text-white -mt-52"
+                      className=" lg:w-[21rem] w-[10rem] h-36 lg:h-40 text-white lg:-mt-52 -mt-36"
                       src={item.image_url}
                       alt={item.feature}
                     />
